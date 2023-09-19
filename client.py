4fda3f80b56848ef39031f341c1062e01ccf9338
StefanoB1109 <112136313+StefanoB1109@users.noreply.github.com>
2023-09-19 13:46:02 -0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,35 @@
+from socket import *
+import time
+
+PORT = 12000
+SERVER = "192.168.1.15"
+ADDR = (SERVER, PORT)
+MAX_BUFFER = 1024
+DISCONNECT = 'quit'
+log = []
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+print(f"[CONNECTING] Connecting to {SERVER}")
+time.sleep(1)
+print("[SUCCESSFUL CONNECTION] Enter 'quit' to disconnect...")
+
+connected = True
+
+while connected:
+    message = input("[SEND MESSAGE]... ")
+    log.append(message)
+
+    if message == DISCONNECT:
+        client_socket.send(message.encode())
+        client_socket.close()
+        print("[LOG]...")
+
+        for x in log:
+            print(x)
+        break
+
+    print(f"[SENDING]... Sending message to {SERVER}: {message}")
+    client_socket.send(message.encode())
+
---
+++ server.py
@@ -0,0 +1,39 @@
+from socket import *
+import time
+import threading
+
+PORT = 12000
+SERVER = "192.168.1.15"
+ADDR = (SERVER, PORT)
+MAX_BUFFER = 1024
+DISCONNECT = 'quit'
+
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.bind(ADDR)
+server_socket.listen(1)
+
+print("[STARTING] Server is starting...")
+time.sleep(1)
+print(f"[READY] Server is ready to recieve at {SERVER}...")
+
+def connect_client():
+    while True:
+        connection, addr = server_socket.accept()
+        thread = threading.Thread(target = client_message, args = (connection, addr))
+        thread.start()
+        
+def client_message(connection, addr):
+    print(f"[SUCCESSFUL CONNECTION] {addr}")
+
+    connected =  True
+    while connected:
+        message = connection.recv(MAX_BUFFER).decode()
+        if message == DISCONNECT:
+            connected = False
+            print(f"[DISCONNECTED] {addr} has been disconnected...")
+            connection.close()
+        else: 
+            print(f"[MESSAGE RECIEVED] {addr}: {message}")
+
+connect_client()
+
